f039182037b58ba6541a8ba6a3ae393fa2e849ad
NachinBombin <solidnachox@gmail.com>
2026-05-03 02:48:30 -0400

    Delete lua directory

--- lua/entities/ent_bombin_support_b52/shared.lua
+++
@@ -1,6 +0,0 @@
-ENT.Type            = "anim"
-ENT.Base            = "base_gmodentity"
-ENT.PrintName       = "Bombin Support B-52 Stratofortress"
-ENT.Author          = "NachinBombin"
-ENT.Spawnable       = false
-ENT.AdminSpawnable  = false
